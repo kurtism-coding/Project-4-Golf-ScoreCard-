33743f52fa0627f9aa7ce6ff474c372b6fc06f33
kmarriott <kurtis.s.marriott@gmail.com>
2017-12-23 18:56:33 -0700

    figured out that I need a json get method

--- myjs.js
+++ myjs.js
@@ -1,16 +1,40 @@
-var closeCourses;
-var local_obj= {latitude:40.4426135, longitute: -111.8631116, radius:100};
+let closeCourses;
+let local_obj= {latitude:40.4426135, longitude: -111.8631116, radius:100};
+//this is the data I'm sending to the API
 
+//this is sending the data to the api, having it return data in the form of JSON, parsing the data into a Javascript
+//object, and then searching that object for the property(actually an array) of "courses" and returning the "p" index
+// of the courses array
+
+//this is totally working!
 function loadMe() {
-    $.post("https://golf-courses-api.herokuapp.com/courses",local_obj,function(data,status){
+    console.log("loadMe called");
+    $.post("https://golf-courses-api.herokuapp.com/courses", local_obj, function (data, status) {
         closeCourses = JSON.parse(data);
-        for (var p in closeCourses.courses){
+        console.log(closeCourses);
+        for (let p in closeCourses.courses) {
             console.log(closeCourses.courses[p].name);
         }
-
     });
 }
+/*function outTotal(){
+    let player1 = document.GetElementById("player1");
+    let player2 = document.GetElementById("player2");
+    let player3 = document.GetElementById("player3");
+    let player4 = document.GetElementById("player4");
+    player1 = $("#p1h1").val();
 
+    for (i = 0; i < 18; i++) {
+        text += cars[i] + "<br>";
+    }
+}
+*/
+function inTotal(player){
+
+}
+function totals(player){
+
+}
 function buildCard(mytee){}
 
 function fillCard(){}
@@ -20,3 +44,4 @@ function deletePlayer(){}
 function getCourse(){}
 
 function updateScore(){}
+loadMe();
