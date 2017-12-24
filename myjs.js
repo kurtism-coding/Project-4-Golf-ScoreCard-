b26e40f5958045d7b1cf491fc63baf813685f88a
kmarriott <kurtis.s.marriott@gmail.com>
2017-12-23 23:26:56 -0700

    done with totals sections, working on api data

--- myjs.js
+++ myjs.js
@@ -1,14 +1,7 @@
 let closeCourses;
 let local_obj = {latitude:40.4426135, longitude: -111.8631116, radius:100};
 let courseId = 11819;
-let p1Out;
-let p2Out;
-let p3Out;
-let p4Out;
-let p1In;
-let p2In;
-let p3In;
-let p4In;
+let courseData;
 
 //this is the data I'm sending to the API
 
@@ -18,38 +11,60 @@ let p4In;
 
 //this is totally working!
 function loadMe() {
-    console.log("loadMe called");
+    //console.log("loadMe called");
     $.post("https://golf-courses-api.herokuapp.com/courses", local_obj, function (data, status) {
         closeCourses = JSON.parse(data);
-        console.log(closeCourses);
+        /*console.log(closeCourses);
         for (let p in closeCourses.courses) {
             console.log(closeCourses.courses[p].name);
-        }
+        }*/
     });
 }
+function onLoad(){
+    for(y=1;y<19;y++){
+        document.getElementById("y" + y).innerHTML = courseData.holes[y].tee_boxes[2];
+    }
+}
+
 function getCourseInfo(){
     $.get("https://golf-courses-api.herokuapp.com/courses/" + courseId, function(data){
-        //console.log(data);
+        courseData = JSON.parse(data)
     })
 }
+
+//not working because my contentEditable's aren't returning a number or string, but an object
 function outTotal(player){
-    console.log("outTotal called");
+    //console.log("outTotal called");
     let i;
-    let outTotal;
-    for (i=0;i<9;i++){
-        outTotal += Number($("#p" + player + "h" + i).val())
+    let outTotal = 0;
+    for (i=0;i<10;i++){
+        let score = "#p" + player + "h" + i;
+        outTotal += Number($(score).text());
+        console.log(outTotal);
     }
-    document.getElementById("player" + player + "Out").innerHTML = outTotal;
+    //console.log(outTotal);
+    document.getElementById("player" + player + "Out").innerHTML = "" + outTotal + "";
 }
 
 function inTotal(player){
-    console.log("inTotal called");
-    document.getElementById("player" + player + "In").innerHTML = ;
-}
-function totals(player){
-    console.log("totals called");
-    document.getElementById("player" + player + "Total").innerHTML = ;
+    //console.log("inTotal called");
+    let i;
+    let inTotal = 0;
+    for (i=10;i<19;i++){
+        let score = "#p" + player + "h" + i;
+        inTotal += Number($(score).text());
+        //console.log("inTotal");
+    }
+    //console.log(inTotal);
+    document.getElementById("player" + player + "In").innerHTML = "" + inTotal + "";
 }
 
-//loadMe();
+function totals(player){
+    console.log("totals called");
+    let total = Number($("#player" + player + "Out").text()) + Number($("#player" + player + "In").text());
+    console.log(total);
+    document.getElementById("player" + player + "Total").innerHTML = "" + total + "";
+}
+loadMe();
+onLoad();
 getCourseInfo();
